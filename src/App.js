7de03977b7843e92c1c165fc7079959c6fdf38ce
Fassari Luigi <fassari.loy@gmail.com>
2021-03-03 20:16:00 +0100

    Add Error

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ import Hero from "./components/Hero";
 import Card from "./components/Card";
 import Footer from "./components/Footer";
 import Loader from "./components/Loader";
+import Error from "./components/Error";
 
 import "./App.css";
 
 /*const fakeProducts = require("./mocks/data/products.json");*/
+
+const fakeProducts = "https://fakestoreapi.com/product";
 
 const data = {
   title: "Edgemony Shop",
@@ -24,33 +27,29 @@ const data = {
 function App() {
   const [ callApi, setCallApi ] = useState([]);
   const [ isLoading, setLoading ] = useState(false);
-
-  /*
-  const [isError, setError] = useState(false);
-  */
+  const [ isError, setError ] = useState(false);
 
   useEffect(() => {
     /*console.log("useEffect");*/
     setLoading(true);
-    /*
-    setError(false);*/
+    setError(false);
 
-    fetch("https://fakestoreapi.com/products")
+    fetch(fakeProducts)
       .then((response) => response.json())
       .then((callApi) => {
-        console.log(callApi)
+        /*console.log(callApi)*/
         data.products = callApi;
         setCallApi(callApi);
         setLoading(false);
       })
-      /*
+      
       .catch(() => {
         setLoading(false);
         setError(true);
+        //let strError = response;
       });
-      */
+      
   }, []);
-
 
   return (
     <div className="App">
@@ -73,8 +72,8 @@ function App() {
             /> 
           : 
             <Loader />
-          
         }
+        { isError && <Error /> }
         </div>
       </main>
       
---
+++ src/components/Error.js
@@ -0,0 +1,11 @@
+import './Error.css'
+
+function Error() {
+  return (
+    <div className="divError"> 
+      <h2>Errore di comunicazione</h2>
+    </div>
+  )
+}
+
+export default Error;
